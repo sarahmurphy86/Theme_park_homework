7113f7f7b691e769b800ee593c1d24b773170051
sarahmurphy86 <sarah.graham86@gmail.com>
2018-05-21 17:40:50 +0100

    customer class set up

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -1,2 +1,23 @@
 public class Customer {
+    private int age;
+    private int height;
+    private double wallet;
+    
+    public Customer(int age, int height, double wallet){
+        this.age = age;
+        this.height = height;
+        this.wallet = wallet;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public double getWallet() {
+        return wallet;
+    }
 }
--- src/test/java/CustomerTest.java
+++ src/test/java/CustomerTest.java
@@ -1,2 +1,31 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CustomerTest {
+
+    Customer adult;
+    Customer child;
+
+    @Before
+    public void before(){
+        adult = new Customer(35, 140, 120.00);
+        child = new Customer( 10, 100, 5.50);
+    }
+
+    @Test
+    public void hasAge(){
+        assertEquals( 35, adult.getAge());
+    }
+
+    @Test
+    public void hasHeight(){
+        assertEquals(100, child.getHeight());
+    }
+
+    @Test
+    public void hasWallet(){
+        assertEquals(5.50, child.getWallet(), 0.01);
+    }
 }
